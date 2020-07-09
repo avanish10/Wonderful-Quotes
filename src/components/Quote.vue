40250b5a4eda53bca49dd15f402071e157afdd81
avanish singhal <avanishsinghal149@gmail.com>
2020-07-09 20:54:20 +0530

    first commit

---
+++ src/components/Quote.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="col-sm-6 col-md-4 col-lg-3">
+    <div class="panel panel-default">
+      <div class="panel-body quote">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.panel-body {
+  font-family: "Tangerine", cursive;
+  font-size: 24px;
+  color: blue;
+  border: 1px solid black;
+}
+.quote {
+  cursor: pointer;
+}
+.quote:hover {
+  background-color: lightsalmon;
+}
+</style>
